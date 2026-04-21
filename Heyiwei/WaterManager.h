2494fe462ec3bade945746c1323a93d8c7a095a8
baiyang1231 <2501615752@qq.com>
2026-04-21 22:26:38 +0800

    白杨的注释1

--- Heyiwei/WaterManager.h
+++ Heyiwei/WaterManager.h
@@ -1,25 +1,25 @@
-#pragma once
+#pragma once // 以.h为结尾的头文件中添加防止这个头文件在编译的过程中重复编译导致报错
 #include <vector>
 #include <string>
 #include "Student.h"
 
 struct result {
-	bool success;
-	std::string info;
+	bool success; // 利用布尔值判断成功与否
+	std::string info; // 利用一个字符串字段来对成功与否的说明
 };
 
 // 水费管理类的声明
 class WaterManager
 {
-private:
-	std::vector<Student> students;
-	const double PRICE_PER_TON = 2.5;
+private: // 外部函数无法调用部分
+	std::vector<Student> students; // 定义一个用于储存每个学生数据的动态数组
+	const double PRICE_PER_TON = 2.5; // 定义常量水费
 
-	int findStudentIndex(const std::string& id);
+	int findStudentIndex(const std::string& id); // 传递id这个数据用传递地址的方法
 	void saveToFile();
 	void loadFromFile();
 
-public:
+public: // 外部函数可以调用部分
 	WaterManager();
 	~WaterManager();
 
